8cd7efa10e9d9d86b0a2d0da75a8644d6fc812af
Victor <lessricc@gmail.com>
2024-05-07 21:56:38 +0200

    ya comenzamos

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+import imageClose from '../assets/images/close.svg';
+import imageMenu from '../assets/images/hamburger.svg';
+
+const buttonElement = document.getElementById('button-menu');
+const menuElement = document.getElementById('menu');
+
+const handleMenu = () => {
+  if (menuElement.classList.contains('menu--show')) {
+    menuElement.classList.remove('menu--show');
+    document.body.classList.remove('bg-opacity');
+    buttonElement.src = imageMenu;
+  } else {
+    menuElement.classList.add('menu--show');
+    document.body.classList.add('bg-opacity');
+    buttonElement.src = imageClose;
+  }
+};
+
+buttonElement.addEventListener('click', handleMenu);
